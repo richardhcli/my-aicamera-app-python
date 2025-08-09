708d36e4b697a4ec90501ebfaf40f1b7d3c7fcec
Richard Li <rhli.richardli@gmail.com>
2025-08-09 18:29:59 -0400

    working basic fastapi image server
    basic python script that can display a new image at raspberrypi.local:8000/image via fastapi serving a image

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from picamera2 import Picamera2, Preview
+import io
 
-picam2 = Picamera2()
-picam2.start_preview(Preview.NULL)
-picam2.start_and_capture_file("test.jpg")
+from picamera2 import Picamera2
+from fastapi import FastAPI
+from fastapi.responses import Response
+
+
+app = FastAPI()
+
+
+@app.get("/image")
+def get_image():
+    picam2 = Picamera2()
+    capture_config = picam2.create_still_configuration(main={"size": (1920, 1080)})
+    picam2.configure(capture_config)
+    data = io.BytesIO()
+    picam2.start()
+    picam2.capture_file(data, format="jpeg")
+    picam2.stop()
+    picam2.close()
+    return Response(content=data.getvalue(), media_type="image/jpeg")
 
